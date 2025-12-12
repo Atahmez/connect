03eccce4b86044b85fcd3beebe15bc7e444c703c
Arik Patel <112505943+Atahmez@users.noreply.github.com>
2025-12-12 10:35:14 -0500

    Rename passwordHash to password in UserEntity
    Updated UserEntity to change passwordHash to password.

--- app/src/main/java/com/example/connectu/Data/UserEntity.kt
+++ app/src/main/java/com/example/connectu/Data/UserEntity.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class UserEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val username: String,
-    val passwordHash: String,
-    val displayName: String
+    val password: String
 )
